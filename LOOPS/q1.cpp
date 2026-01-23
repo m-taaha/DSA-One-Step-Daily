a763d6373911cb01314e9416820a6bbec17ab4ff
Mohammad Taaha Ashraf <mohammadtaahaashraf@gmail.com>
2026-01-23 16:47:47 +0530

    loop q1t

---
+++ LOOPS/q1.cpp
@@ -0,0 +1,19 @@
+// 1. First 10 Natural Numbers
+
+// Write a program in C++ to find the first 10 natural numbers.
+
+// Sample output:
+// The natural numbers are:
+// 1 2 3 4 5 6 7 8 9 10
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 10;
+    for (int i = 0; i <= n; i++) {
+        cout << i << endl;
+    }
+
+    return 0 ;
+}
